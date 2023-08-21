80e7eea84a649ef5ae983368a13b66b8640cc5f1
Dennis Aldana <dennis.aldana@gmail.com>
2023-08-20 22:48:47 -0600

    fix: better simple tiling

--- src/Pong/Systems.cpp
+++ src/Pong/Systems.cpp
@@ -108,9 +108,6 @@ void TilemapSetupSystem::run() {
   float offsetY = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
   float zoom = 20.0f;
 
-  Terrain grass{grassTexture};
-  Terrain water{waterTexture};  
-
   for (int y = 0; y < tilemapComponent.height; y++) {
       for (int x = 0; x < tilemapComponent.width; x++) {
           float factor = noise.GetNoise(
@@ -122,12 +119,9 @@ void TilemapSetupSystem::run() {
           Tile& tile = tilemapComponent.tilemap[index];
 
           if (factor < 0.5) {
-            tile.up = grass;
-            tile.down = water;
-            tile.needsAutoTiling = true;
-          } else{
-            tile.up = water;
-            tile.needsAutoTiling = false;
+            tile.texture = grassTexture;
+          } else {
+            tile.texture = waterTexture;
           }
         }
     }
@@ -143,7 +137,7 @@ void TilemapRenderSystem::run(SDL_Renderer* renderer) {
   for (int y = 0; y < height; y++) {
     for (int x = 0; x < width; x++) {
       Tile& tile = tilemapComponent.tilemap[y * width + x];
-      tile.up.texture->render(
+      tile.texture->render(
           x * size * scale,
           y * size * scale,
           size * scale,
@@ -152,58 +146,4 @@ void TilemapRenderSystem::run(SDL_Renderer* renderer) {
     }
   }
 }
-/*
-// run method for the AutoTilingUpdateSystem class
-void AutoTilingSetupSystem::run() {
-    // Get the TilemapComponent from the scene
-    const auto& tilemapComponent = scene->world->get<TilemapComponent>();
 
-    // Define the delta x and y for each of the eight directions (N, NE, E, SE, S, SW, W, NW)
-    const int dx[8] = { 0, 1, 1, 1, 0, -1, -1, -1 };
-    const int dy[8] = { -1, -1, 0, 1, 1, 1, 0, -1 };
-
-    // Loop through each tile in the tilemap
-    for (int y = 0; y < tilemapComponent.height; y++) {
-        for (int x = 0; x < tilemapComponent.width; x++) {
-
-            // Calculate the index of the current tile in the tileEntities vector
-            int index = y * tilemap.width + x;
-
-            // Get the Entity and AutoTilingInfo of the current tile
-            auto& entity = tilemap.tileEntities[index];
-            auto& info = entity.get<AutoTilingInfo>();
-
-            // Reset the surrounding field of the AutoTilingInfo
-            info.surrounding = 0;
-
-            // Loop through each of the eight directions
-            for (int i = 0; i < 8; i++) {
-
-                // Calculate the coordinates of the neighboring tile
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                // Check if the coordinates are out of bounds
-                if (nx < 0 || nx >= tilemap.width || ny < 0 || ny >= tilemap.height) {
-                    continue;  // If out of bounds, skip this iteration
-                }
-
-                // Calculate the index of the neighboring tile in the tileEntities vector
-                int neighborIndex = ny * tilemap.width + nx;
-
-                // Get the Tile of the neighboring tile
-                auto& neighborTile = tilemap.tileEntities[neighborIndex].get<Tile>();
-
-                // Check if the neighboring tile is the same type as the current tile
-                if (neighborTile.isWater == entity.get<Tile>().isWater) {
-
-                    // If the neighboring tile is the same type, set the corresponding bit in the surrounding field
-                    info.surrounding |= (1 << i);
-                }
-            }
-
-            // TODO: update the entity's tile based on info.surrounding
-        }
-    }
-}
-*/
